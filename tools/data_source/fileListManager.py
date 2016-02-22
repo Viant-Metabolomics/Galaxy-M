adec4806e3d989ddae2d03054f91fbffc8bef1ae
RJMW <r.j.weber@bham.ac.uk>
2016-02-22 14:13:58 +0000

    Update Tool: FileListManager

--- tools/data_source/fileListManager.py
+++ tools/data_source/fileListManager.py
@@ -10,7 +10,7 @@ import xml.etree.ElementTree as etree
 import time, datetime, sys
 from xml.dom import minidom
 
-def WriteXML(pars, instrument, fnCSV, fnOut):
+def WriteXML(pars, fnCSV, fnOut):
 
     root = etree.Element('fileList')
     TimeStamp = str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
@@ -21,10 +21,6 @@ def WriteXML(pars, instrument, fnCSV, fnOut):
     rootDirectory = etree.SubElement(root, 'RootDirectory')
     rootDirectory.set("type", "text")
     rootDirectory.text=pars[0].replace("__at__", "@")
-
-    instrumentType = etree.SubElement(root, 'Instrument')
-    instrumentType.set("type", "text")
-    instrumentType.text = instrument
 
     spec = etree.SubElement(root, 'Samples')
     spec.set("type", "struct")
@@ -49,24 +45,14 @@ def WriteXML(pars, instrument, fnCSV, fnOut):
                 dataFile = etree.SubElement(instance, 'dataFile')
                 dataFile.set("type", "text")
 
-                if instrument == "ltqft" or instrument == "orbitrap" or instrument == "qexactive":
-                    if ".raw" in row[0] or ".RAW" in row[0]:
-                        dataFile.text = row[0]
-                        ID.text = row[0].rstrip(".raw").rstrip(".RAW")
+                if ".raw" in row[0] or ".RAW" in row[0]:
+                    ID.text = row[0].rstrip(".raw").rstrip(".RAW")
+                elif ".d" in row[0] or ".D" in row[0]:
+                    ID.text = row[0].rstrip(".d").rstrip(".D")  
+                elif ".mzML" in row[0] or ".mzml" in row[0] or ".MZML" in row[0]:
+                    ID.text = row[0].rstrip(".mzml").rstrip(".mzML").rstrip(".MZML") 
+                dataFile.text = row[0]
 
-                    else:
-                        dataFile.text = "%s.RAW" % (row[0])
-                        ID.text = row[0]
-
-                elif instrument == "solarix":
-                    if ".d" in row[0] or ".D" in row[0]:
-                        dataFile.text = row[0]
-                        ID.text = row[0].rstrip(".d").rstrip(".D")
-                        
-                    else:
-                        dataFile.text = "%s.d" % (row[0])
-                        ID.text = row[0]
-                
                 sampleID = etree.SubElement(instance,'sampleID')
                 sampleID.set("type", "text")
                 sampleID.text = row[1]
@@ -107,17 +93,13 @@ def main():
     parser.add_argument('--outfile',help='Output XML file', required=True, default='filelist.xml')
     parser.add_argument('--dir', help='Directory of Data files', default=None)
     parser.add_argument('--numreps', help='Number of technical replicates', default=3, type = int)
-    parser.add_argument('--instrument',help='Instrument Name (QExactive; Orbitrap or LTQFT; or Solarix)', required=True)
     
     args = parser.parse_args()
     
-    if args.instrument.lower() not in ["qexactive", "orbitrap", "ltqft", "solarix"]:
-        print "Instrument name not available"
-
     if args.dir[-1]!='/':
         args.dir = args.dir+'/'
     
-    WriteXML([args.dir, args.numreps], args.instrument.lower(), args.incsv, args.outfile)
+    WriteXML([args.dir, args.numreps], args.incsv, args.outfile)
 
 if __name__ == "__main__":
     main()
